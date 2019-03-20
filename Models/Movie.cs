4d8acca2df02eca77fa754b1053bc77233331869
Lyes HADJ AISSA <lyesha526@gmail.com>
2019-03-20 10:42:17 -0400

    commit

---
+++ Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovies.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [StringLength(60,MinimumLength =3,ErrorMessage ="It must be between 3 and 60")]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        public string Rating { get; set; }
+    }
+}
